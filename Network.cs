0d227a9c190f768b23d62292c356f360ff34ef56
Vismaya908 <106880013+Vismaya908@users.noreply.github.com>
2022-06-05 23:44:04 +0530

    Add files via upload

---
+++ Network.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace encryption
+{
+    class Network
+    {
+
+        public static string getHostIP()
+        {
+            IPAddress[] ipa = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress ip in ipa)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    return ip.ToString();
+            }
+
+            return "192.168.0.12";
+        }
+    }
+}
