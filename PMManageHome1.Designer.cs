878e6306dc5c824209d9dab972e7ef874a3f6356
Vismaya908 <106880013+Vismaya908@users.noreply.github.com>
2022-06-06 09:47:12 +0530

    Add files via upload

---
+++ PMManageHome1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace encryption
+{
+    partial class PMManageHome1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton10 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.White;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton6,
+            this.toolStripButton1,
+            this.toolStripButton7,
+            this.toolStripButton10,
+            this.toolStripButton9,
+            this.toolStripButton8,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 684);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStrip1.Size = new System.Drawing.Size(1128, 71);
+            this.toolStrip1.TabIndex = 23;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton6.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton6.Image = global::encryption.Properties.Resources.icons8_exit_64__1_;
+            this.toolStripButton6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton6.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(191, 68);
+            this.toolStripButton6.Text = "Shutdown";
+            this.toolStripButton6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton1.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton1.Image = global::encryption.Properties.Resources.icons8_file_directory_64;
+            this.toolStripButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(224, 68);
+            this.toolStripButton1.Text = "File Directory";
+            this.toolStripButton1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton7.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton7.Image = global::encryption.Properties.Resources.icons8_download_64__2_;
+            this.toolStripButton7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton7.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Violet;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(191, 68);
+            this.toolStripButton7.Text = "Download";
+            this.toolStripButton7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton7.Click += new System.EventHandler(this.toolStripButton7_Click);
+            // 
+            // toolStripButton10
+            // 
+            this.toolStripButton10.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton10.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton10.Image = global::encryption.Properties.Resources.icons8_file_upload_64;
+            this.toolStripButton10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton10.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton10.ImageTransparentColor = System.Drawing.Color.Violet;
+            this.toolStripButton10.Name = "toolStripButton10";
+            this.toolStripButton10.Size = new System.Drawing.Size(159, 68);
+            this.toolStripButton10.Text = "Upload";
+            this.toolStripButton10.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton10.Click += new System.EventHandler(this.toolStripButton10_Click);
+            // 
+            // toolStripButton9
+            // 
+            this.toolStripButton9.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton9.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton9.Image = global::encryption.Properties.Resources.assignment;
+            this.toolStripButton9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton9.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton9.Name = "toolStripButton9";
+            this.toolStripButton9.Size = new System.Drawing.Size(214, 68);
+            this.toolStripButton9.Text = "Assign Work";
+            this.toolStripButton9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton9.Click += new System.EventHandler(this.toolStripButton9_Click);
+            // 
+            // toolStripButton8
+            // 
+            this.toolStripButton8.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton8.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton8.Image = global::encryption.Properties.Resources.delete;
+            this.toolStripButton8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton8.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton8.Name = "toolStripButton8";
+            this.toolStripButton8.Size = new System.Drawing.Size(207, 68);
+            this.toolStripButton8.Text = "Delete Emp";
+            this.toolStripButton8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton8.Click += new System.EventHandler(this.toolStripButton8_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.toolStripButton5.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripButton5.Image = global::encryption.Properties.Resources.add__1_;
+            this.toolStripButton5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripButton5.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(180, 68);
+            this.toolStripButton5.Text = "Add Emp";
+            this.toolStripButton5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // PMManageHome1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::encryption.Properties.Resources._123;
+            this.ClientSize = new System.Drawing.Size(1128, 755);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PMManageHome1";
+            this.Text = "PMManageHome1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+        private System.Windows.Forms.ToolStripButton toolStripButton10;
+        private System.Windows.Forms.ToolStripButton toolStripButton9;
+        private System.Windows.Forms.ToolStripButton toolStripButton8;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
---
+++ PMManageHome1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace encryption
+{
+    public partial class PMManageHome1 : Form
+    {
+        public PMManageHome1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            AddEmpDetails obj = new AddEmpDetails();
+            obj.Show();
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+            DeleteEmployee obj = new DeleteEmployee();
+            obj.Show();
+        }
+
+        private void toolStripButton9_Click(object sender, EventArgs e)
+        {
+            PMAssignWork obj = new PMAssignWork();
+            obj.Show();
+        }
+
+        private void toolStripButton10_Click(object sender, EventArgs e)
+        {
+            PMUploadFile obj = new PMUploadFile();
+            obj.Show();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            PMDownloadFile obj = new PMDownloadFile();
+            obj.Show();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            FileDirectoryView obj = new FileDirectoryView();
+            obj.Show();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
